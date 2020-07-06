d4e5f8bfb0ea63fb1d52133bf4128060c49d14cb
chowravc <chowravc@gmail.com>
2020-07-06 17:07:31 +0530

    Documented validate.py

--- mAP/validate.py
+++ mAP/validate.py
@@ -6,6 +6,20 @@ import json
 import imp
 
 def validateRun(home, mAP, validationTruthPath, validationDetectionPath):
+	"""Validate a trained model on a test set with xml true files and json detection files, result in mAP.
+
+    Args:
+        home (str): home directory of SETT
+        mAP (str): mAP directory
+        validationTruthPath (str): path to true xml files UPDATE
+        validationDetectionPath (str): path to detected json files UPDATE
+
+    Writes:
+        results: results of validation
+
+    Note:
+        UPDATE
+    """
 
     truthPath = os.path.join(home, validationTruthPath) #should be xml
     detectionPath = os.path.join(home, validationDetectionPath) #should be .json
@@ -73,65 +87,3 @@ def validateRun(home, mAP, validationTruthPath, validationDetectionPath):
     sim = imp.load_source('packages', os.path.join(mAPPath,'mAPValDist.py'))
     method = getattr(sim,'mAPVal')
     method(20)
-
-if __name__ == "__main__":
-
-    truthPath = os.path.abspath(config["validation_truth_path"]) #should be xml
-    detectionPath = os.path.abspath(config["validation_detection_path"]) #should be .json
-    mAPPath = os.path.abspath(config["mAPPath"]) #path to your map folder
-
-    mAPTruthPath = os.path.join(mAPPath,"input\\ground-truth\\")
-    #os.path.abspath("C:/Users/Eric Minor/TrackingML/defectTracker/mAP/input/ground-truth/")
-    mAPDetectionPath = os.path.join(mAPPath,"input\\detection-results\\")
-    #os.path.abspath("C:/Users/Eric Minor/TrackingML/defectTracker/mAP/input/detection-results")
-    mAPScriptsPath = os.path.join(mAPPath, "scripts\\extra\\")
-
-
-    def wipeFolder(folder):
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path): shutil.rmtree(file_path)
-            except Exception as e:
-                print(e)
-
-
-
-    wipeFolder(mAPTruthPath)
-    wipeFolder(mAPDetectionPath)
-
-
-    filePattern = 	os.path.join(truthPath,"*.xml")
-    #print(filePattern)
-    for filename in glob.glob(filePattern):
-        drive,pathAndFile = os.path.splitdrive(filename)
-        path, file = os.path.split(pathAndFile)
-        newFilename = os.path.join(mAPTruthPath,file)
-        shutil.copyfile(filename,newFilename)
-        #print(filename)
-        
-        
-    filePattern = 	os.path.join(detectionPath,"*.json")
-    #print(filePattern)
-    for filename in glob.glob(filePattern):
-        drive,pathAndFile = os.path.splitdrive(filename)
-        path, file = os.path.split(pathAndFile)
-        newFilename = os.path.join(mAPDetectionPath,file)
-        shutil.copyfile(filename,newFilename)
-        
-    sys.path.append(mAPScriptsPath)
-    os.chdir(mAPScriptsPath)
-    print(os.getcwd())
-    from convert_dr_darkflow_jsonF import convertJSON
-    from convert_gt_xmlF import convertXML
-    convertJSON()
-    convertXML()
-
-    sys.path.append(mAPPath)
-    os.chdir(mAPPath)
-
-    from mAPValDist import mAPVal
-
-    mAPVal(20)
