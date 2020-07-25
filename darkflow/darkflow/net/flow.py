04ae81136f67a93e01d9bbd8474498810dc7453e
chowravc <chowravc@gmail.com>
2020-07-25 15:25:28 +0530

    Making sure everything is up to date

--- darkflow/darkflow/net/flow.py
+++ darkflow/darkflow/net/flow.py
@@ -69,13 +69,13 @@ def train(self):
 
         ckpt = (i+1) % (self.FLAGS.save // self.FLAGS.batch)
         args = [step_now, profile]
-        #if not ckpt: _save_ckpt(self, *args)
+        if not ckpt: _save_ckpt(self, *args)
 
-    #if ckpt: _save_ckpt(self, *args)
+    if ckpt: _save_ckpt(self, *args)
 
 def return_predict(self, im):
     assert isinstance(im, np.ndarray), \
-				'Image is not a np.ndarray'
+                'Image is not a np.ndarray'
     h, w, _ = im.shape
     im = self.framework.resize_input(im)
     this_inp = np.expand_dims(im, 0)
